059bf249221750e669d3ffb09f685d2263ac64d7
Lance Goodridge <ldgoodridge95@gmail.com>
2017-12-10 01:44:29 -0500

    Replaced parser layer with capturer layer
    Coalesced json packet parsing code into json_parser.py
    Renamed parser directory to capturer

--- json_parser.py
+++ json_parser.py
@@ -1,3 +1,9 @@
+"""
+JSON implementation of the TSA Packet and wireshark parser
+modules. Left here because some of this code can probably
+be reused in the current implementation.
+"""
+
 import json
 
 # Packet Data class, container for different packet information. For now only contains ip and tcp/udp information.
@@ -65,3 +71,44 @@ class WiresharkPacket:
 
     def __repr__(self):
         return "Attributes:\t{}".format(json.dumps(self.__dict__))
+
+
+# Extract packets wireshark json dump. Return an array of packet objects.
+def packets_from_JSON(filename):
+    try:
+        packets_file = open(filename, "r")
+    except IOError as e:
+        raise e
+
+    packet_dict_list = json.load(packets_file)
+
+    # Extract data from json dump to construct packet objects. For now we only extract.
+    packets = []
+    for packet_dict in packet_dict_list:
+        layers = packet_dict["_source"]["layers"]
+        kwargs = {}
+        if "ip" in layers:
+            kwargs["ipv4"] = dict(dst_address=layers["ip"]["ip.dst_host"], src_address=layers["ip"]["ip.src_host"])
+        elif "ipv6" in layers:
+            kwargs["ipv6"] = dict(dst_address=layers["ipv6"]["ipv6.dst_host"], src_address=layers["ipv6"]["ipv6.src_host"])
+
+        if "tcp" in layers:
+            kwargs["tcp"] = dict(dst_port=layers["tcp"]["tcp.dstport"], src_port=layers["tcp"]["tcp.srcport"])
+        elif "udp" in layers:
+            kwargs["udp"] = dict(dst_port=layers["udp"]["udp.dstport"], src_port=layers["udp"]["udp.srcport"])
+
+        packet = WiresharkPacket(**kwargs)
+        packets.append(packet)
+
+    packets_file.close()
+
+    return packets
+
+
+# Test packets_from_JSON, asks for the file name to parse packets from and print out the first 20 packets parsed.
+if __name__ == "__main__":
+    file_name = input("Enter a FileName: ")
+    packets = packets_from_JSON(file_name)
+
+    for i in range(min(20, len(packets))):
+        print(packets[i])
