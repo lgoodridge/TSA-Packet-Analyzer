1c620b39bc6b91dfc1d0d6baaaf979736588219e
firenat13 <natonthemove@gmail.com>
2018-01-12 23:18:09 -0500

    aggregated domain names and counts

--- analyzer/dns.py
+++ analyzer/dns.py
@@ -2,6 +2,7 @@
 This module contains dns / hostname related analysis functions.
 """
 
+from tld import get_tld
 from analyzer.utils import get_host_ip_addr
 
 def get_fqdn_to_packet_count(packets):
@@ -37,14 +38,57 @@ def get_fqdn_to_packet_count(packets):
 
     ip_counts.pop(host_ip_addr, None)
 
-    fqdn_counts = {}
-    fqdn_counts[unknown] = 0
+    # Keeps track of counts per domain
+    fqdn_domain_counts = {}
+    fqdn_domain_counts[unknown] = 0
+    # Keeps track of aliases per domain
+    fqdn_domain_aliases = {}
+    fqdn_domain_aliases[unknown] = {unknown}
+
     for ip, count in ip_counts.items():
         fqdns = ip_to_fqdns.get(ip, None)
 
         if fqdns:
-            fqdn_counts[", ".join(fqdns)] = count
-        else:
-            fqdn_counts[unknown] += 1
+            domain_set = set()
+            for fqdn in fqdns:
+                res = get_tld(fqdn, as_object=True, fail_silently=True,
+                              fix_protocol=True)
+                domain_set.add(str(res))
+            domain_set = list(domain_set)
+            # Add domains to domain counts, only adding first entry if multiple
+            domain = domain_set[0]
+            if domain in fqdn_domain_counts:
+                fqdn_domain_counts[domain] += count
+            else:
+                fqdn_domain_counts[domain] = count
 
-    return fqdn_counts
+            # Add aliases for domains
+            for domain1 in domain_set:
+                if domain1 not in fqdn_domain_aliases:
+                    fqdn_domain_aliases[domain1] = {domain1}
+                for domain2 in domain_set:
+                    if domain2 not in fqdn_domain_aliases[domain1]:
+                        if (domain2 in fqdn_domain_aliases):
+                            for domain in fqdn_domain_aliases[domain2]:
+                                if domain != domain1:
+                                    if (domain in fqdn_domain_aliases):
+                                        fqdn_domain_aliases[domain] = fqdn_domain_aliases[domain].union(fqdn_domain_aliases[domain1])
+                                        fqdn_domain_aliases[domain1] = fqdn_domain_aliases[domain].union(fqdn_domain_aliases[domain1])
+                        fqdn_domain_aliases[domain1].add(domain2)
+            
+                    
+        else:
+            fqdn_domain_counts[unknown] += 1
+
+    fqdn_alias_count = {}    
+    for domain in fqdn_domain_counts:
+        alias_list = list(fqdn_domain_aliases[domain])
+        alias_list.sort()
+        alias_name = ", ".join(alias_list)
+        if alias_name in fqdn_alias_count:
+            fqdn_alias_count[alias_name] += fqdn_domain_counts[domain]
+        else:
+            fqdn_alias_count[alias_name] = fqdn_domain_counts[domain]
+            
+
+    return fqdn_alias_count
